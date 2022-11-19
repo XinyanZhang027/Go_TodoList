48699e569864ce0fadce7734152365e08db27ea1
XinyanZhang027 <lefestin@yahoo.com>
2022-11-19 11:06:55 -0800

    day2

--- main.go
+++ main.go
@@ -2,7 +2,14 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	//_ "github.com/jinzhu/gorm/dialects/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 	"net/http"
+)
+
+var (
+	DB *gorm.DB
 )
 
 type Todo struct {
@@ -11,10 +18,34 @@ type Todo struct {
 	Status bool   "json:'status'"
 }
 
+/*
+	func initMySQL() (err error) {
+		dsn := "go_admin:123456@tcp(127.0.0.1:3306)/go_demo?charset=utf8mb4&parseTime=True&loc=Local"
+		DB, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		//DB, err := gorm.Open(mysql.Open(dsn))
+		//DB, err = gorm.Open("mysql", dsn)
+		if err != nil {
+			return
+		}
+		// 测试DB连通性
+		return DB.DB().ping()
+
+}
+*/
 func main() {
 	// 创建数据库
 	// sql: CREATE DATABASE bubble;
 	// 连接数据库
+	dsn := "go_admin:123456@tcp(127.0.0.1:3306)/go_demo?charset=utf8mb4&parseTime=True&loc=Local"
+	DB, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	//err := initMySQL()
+	if err != nil {
+		panic(err)
+	}
+	//defer DB.Close()
+
+	// 绑定模型
+	DB.AutoMigrate(&Todo{})
 
 	r := gin.Default()
 	// 告诉gin框架模板文件引用的静态文件去哪里找
