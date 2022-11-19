3b2ce09260e4cb9803a87df49836413dfba69cad
BarryLee <barryalan@163.com>
2022-11-18 23:20:48 -0800

    day1

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	// 告诉gin框架模板文件引用的静态文件去哪里找
+	r.Static("/static", "static")
+	// 告诉gin框架去哪里找模板文件
+	r.LoadHTMLGlob("templates/*")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+	r.Run()
+
+}
